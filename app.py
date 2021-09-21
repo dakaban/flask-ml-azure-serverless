0700d6f31f16f0670c1dfd3fe1fea38898a405ce
dana kaban <dakaban@microsoft.com>
2021-09-21 19:30:47 +0000

    test simple app

--- app.py
+++ app.py
@@ -60,12 +60,13 @@ def predict():
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
-    json_payload = request.json
+    prediction = [20]
+    '''json_payload = request.json
     LOG.info("JSON payload: %s json_payload")
     inference_payload = pd.DataFrame(json_payload)
     LOG.info("inference payload DataFrame: %s inference_payload")
     scaled_payload = scale(inference_payload)
-    prediction = list(clf.predict(scaled_payload))
+    prediction = list(clf.predict(scaled_payload))'''
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
